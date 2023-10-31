a9f75163eb02831e6cf4b528836e86ca91631092
Ben Bueltmann <benbueltmann@gmail.com>
2023-10-31 11:30:21 -0500

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Ben Bueltmann on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
